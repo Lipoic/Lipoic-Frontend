ebe628de0c88335be5effaf7d5f406d2e0dc9870
banahaker <pn0818x@outlook.com>
2022-05-02 20:09:29 +0800

    feat: router

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory } from "vue-router"
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import("../views/home.vue")
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: () => import("../views/about.vue")
+    },
+    {
+        path: '/account',
+        name: 'Account',
+        component: () => import("../views/auth.vue")
+    },
+    {
+        path: '/feature',
+        name: 'Features',
+        component: () => import("../views/feature.vue")
+    }
+]
+
+export default createRouter({
+    history: createWebHashHistory(),
+    routes
+})
