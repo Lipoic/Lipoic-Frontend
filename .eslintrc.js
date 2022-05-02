3a9340c1d27222ddcc3dcf66d422880cd2e99088
SiongSng <rrt467778@gmail.com>
2022-05-02 23:29:59 +0800

    fix: router

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/essential",
+    "plugin:@typescript-eslint/recommended",
+    "@vue/typescript/recommended"
+  ],
+  parserOptions: {
+    "ecmaVersion": "latest",
+    "parser": "@typescript-eslint/parser",
+    "sourceType": "module"
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  parser: "vue-eslint-parser",
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
