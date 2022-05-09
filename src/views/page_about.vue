c622a2388203c300d81049519e345e0f8ff868fd
SiongSng <rrt467778@gmail.com>
2022-05-09 18:21:57 +0800

    feat: footer

--- src/views/page_about.vue
+++ src/views/page_about.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import blockHeaderVue from '@/components/block_header.vue';
+import blockFooterVue from '@/components/block_footer.vue';
 </script>
 
 <template>
   <div id="about">
     <blockHeaderVue></blockHeaderVue>
+    <blockFooterVue></blockFooterVue>
   </div>
 </template>
--- src/views/page_demo.vue
+++ src/views/page_demo.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import blockHeaderVue from '@/components/block_header.vue';
+import blockFooterVue from '@/components/block_footer.vue';
 </script>
 
 <template>
   <div id="demo">
     <blockHeaderVue></blockHeaderVue>
+    <blockFooterVue> </blockFooterVue>
   </div>
 </template>
 
