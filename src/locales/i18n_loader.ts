cb8d66a9d3ea060e51dfc3de9a4c0e2ddce35607
SiongSng <rrt467778@gmail.com>
2022-05-14 08:23:44 +0800

    fix: build

--- src/locales/i18n_loader.ts
+++ src/locales/i18n_loader.ts
@@ -4,12 +4,16 @@ class i18nLoader {
   static defaultLocale = 'en_us';
   static locales: Array<string> = [this.defaultLocale, 'zh_cn', 'zh_tw'];
 
-  static async load(): Promise<
-    I18n<Record<string, Record<string, string>>, unknown, unknown, true>
+  static load(): I18n<
+    Record<string, Record<string, string>>,
+    unknown,
+    unknown,
+    true
   > {
     const i18nMessages: Record<string, Record<string, string>> = {};
     for (const locale of this.locales) {
-      i18nMessages[locale] = (await import(`./${locale}.json`)).default;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      i18nMessages[locale] = (import(`./${locale}.json`) as any).default;
     }
 
     return createI18n({
