1fbedf476333275bd74c65dc673f5c2a559f3b47
猴子 <70706886+a3510377@users.noreply.github.com>
2022-05-14 08:27:41 +0800

    fix/use async

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import router from '@/router';
 import '@/scss/global.scss';
 import i18nLoader from './locales/i18n_loader';
 
-const i18n = i18nLoader.load();
+const i18n = await i18nLoader.load();
 
 createApp(App).use(router).use(i18n).mount('#app');
