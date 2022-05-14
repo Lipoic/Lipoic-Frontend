5aaf0da04383f6bf041c4b5b721a4af4379bcf8a
猴子 <70706886+a3510377@users.noreply.github.com>
2022-05-14 08:42:43 +0800

    fix/use import from load locale json

--- src/locales/i18n_loader.ts
+++ src/locales/i18n_loader.ts
@@ -1,18 +1,25 @@
 import { I18n, createI18n } from 'vue-i18n';
 
+import en_us from './en_us.json';
+import zh_cn from './zh_cn.json';
+import zh_tw from './zh_tw.json';
+
 class i18nLoader {
   static defaultLocale = 'en_us';
-  static locales: Array<string> = [this.defaultLocale, 'zh_cn', 'zh_tw'];
+  static i18nFileValues = {
+    en_us,
+    zh_cn,
+    zh_tw,
+  };
+  static locales: Array<string> = Object.keys(this.i18nFileValues);
 
   static async load(): Promise<
     I18n<Record<string, Record<string, string>>, unknown, unknown, true>
   > {
     const i18nMessages: Record<string, Record<string, string>> = {};
 
-    for (const locale of this.locales) {
-      i18nMessages[locale] = (
-        await import(`/src/locales/${locale}.json`)
-      ).default;
+    for (const [language, i18nData] of Object.entries(this.i18nFileValues)) {
+      i18nMessages[language] = i18nData;
     }
 
     return createI18n({
