0525b51c955f641aa68da610ada7ea6d2a07e2fb
banahaker <pn0818x@outlook.com>
2022-05-15 23:14:01 +0800

    feat: create login page

---
+++ src/components/BlockLoginPage.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="login">
+    <div class="left">
+      <div class="mask" v-for="_ in 4" :key="_"></div>
+    </div>
+    <div class="right">
+      <h1>LOGIN</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '@/scss/global.scss';
+.login {
+  background-color: #4d4d4d;
+
+  width: 100%;
+  height: calc(100% - $FooterHeight - $HeaderHeight);
+  display: flex;
+}
+</style>
--- src/views/PageAuth.vue
+++ src/views/PageAuth.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup>
 import MainViewVue from '@/components/MainView.vue';
+import BlockLoginPageVue from '@/components/BlockLoginPage.vue';
 </script>
 
 <template>
-  <MainViewVue id="account"></MainViewVue>
+  <MainViewVue id="account">
+    <BlockLoginPageVue />
+  </MainViewVue>
 </template>
 
 <style lang="scss" scoped></style>
