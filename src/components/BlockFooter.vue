108f9f84ea36a73afd31bd6281a6e4af3a36ded7
SiongSng <rrt467778@gmail.com>
2022-05-15 09:09:31 +0800

    fix: language selector css

--- src/components/BlockFooter.vue
+++ src/components/BlockFooter.vue
@@ -20,7 +20,7 @@ const changeLanguage = () => I18nHelper.setLocale(selectedLanguage.value);
     </div>
     <div class="copyright">{{ $t('footer.copyright') }}</div>
     <div class="languageSelector">
-      {{ $t('footer.language') }}
+      <span>{{ $t('footer.language') }}</span>
       <select v-model="selectedLanguage" @change="changeLanguage">
         <option
           v-for="language in i18n.availableLocales"
@@ -70,11 +70,15 @@ const changeLanguage = () => I18nHelper.setLocale(selectedLanguage.value);
       border-radius: 5px;
       padding: 5px;
       color: $White;
-      font-size: 1.1rem;
-      font-weight: bold;
+      font-size: 1rem;
       &:focus {
         outline: none;
       }
+    }
+
+    span {
+      font-size: 1.2rem;
+      margin-right: 5px;
     }
   }
 }
