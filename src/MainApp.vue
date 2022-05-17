ecc2c3c57832dcf99602e930c0f57352b8875f0a
banahaker <pn0818x@outlook.com>
2022-05-17 20:32:06 +0800

    fix: typo and comments.

--- src/MainApp.vue
+++ src/MainApp.vue
@@ -4,6 +4,7 @@ import { onMounted, onUnmounted } from 'vue';
 const setStyle = (key: `--${string}`, value: string | null) =>
   document.documentElement.style.setProperty(key, value);
 
+// For Safari support because there are a bug of vh and vw unit of Safari.
 const setStyles = () => {
   setStyle('--page-height', `${window.innerHeight}px`);
   setStyle('--page-width', `${window.innerWidth}px`);
