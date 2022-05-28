11223e313d1f13fe6c39b03df787d42f159250c9
猴子 <70706886+a3510377@users.noreply.github.com>
2022-05-28 12:27:32 +0800

    fix

--- plugin/index.ts
+++ plugin/index.ts
@@ -2,13 +2,11 @@ import { Plugin } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 import svgIcon from './svgIcon';
-// import imagemin from './imagemin';
 
 export const loadVitePlugins = () => {
   const vitePlugins: Array<Plugin[] | Plugin> = [vue()];
 
   vitePlugins.push(svgIcon());
-  // vitePlugins.push(imagemin());
 
   return vitePlugins;
 };
