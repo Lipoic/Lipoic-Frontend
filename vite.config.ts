5328e8717d84e434b59ac2f64c1b0485bd1639b6
猴子 <70706886+a3510377@users.noreply.github.com>
2022-05-31 08:04:42 +0800

    feat: add build pref

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,32 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'url';
+import viteImagemin from 'vite-plugin-imagemin';
 
 import vue from '@vitejs/plugin-vue';
 import svgIcon from './plugin/svgIcon';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgIcon()],
+  plugins: [
+    vue(),
+    svgIcon(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 20 },
+      pngquant: { quality: [0.8, 0.9], speed: 4 },
+      svgo: {
+        plugins: [
+          { name: 'removeViewBox' },
+          { name: 'removeEmptyAttrs', active: false },
+        ],
+      },
+    }),
+  ],
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
   build: {
     terserOptions: {
@@ -18,11 +37,20 @@ export default defineConfig({
         keep_infinity: true,
         drop_console: false,
       },
+      format: { comments: false },
     },
     minify: 'terser',
     // Speed up packing
     brotliSize: false,
+    sourcemap: false,
     chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/js/[name]-[hash].js`,
+        chunkFileNames: `assets/js/[name]-[hash].js`,
+        assetFileNames: `assets/[ext]/[name]-[hash].[ext]`,
+      },
+    },
   },
   test: {
     coverage: {
