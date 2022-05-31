0bbfebdb0253f04892e2025826ce570cee77cda9
猴子 <70706886+a3510377@users.noreply.github.com>
2022-05-31 08:08:43 +0800

    dix: fix config

--- vite.config.ts
+++ vite.config.ts
@@ -14,13 +14,6 @@ export default defineConfig({
     vue(),
     svgIcon(),
     viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: { optimizationLevel: 7 },
-      mozjpeg: { quality: 20 },
-      pngquant: { quality: [0.8, 0.9], speed: 4 },
       svgo: {
         plugins: [
           { name: 'removeViewBox' },
