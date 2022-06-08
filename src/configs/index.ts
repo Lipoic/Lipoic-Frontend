b4abdebc9a0ddc3f7e7071b47fe65b5415640a25
猴子 <70706886+a3510377@users.noreply.github.com>
2022-06-08 21:08:45 +0800

    fix: remove debug server

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,13 +3,10 @@ import httpConfig from './http';
 export interface configsType {
   /** There are settings for requests */
   http: typeof httpConfig;
-  /** send debug to server */
-  debugServer: boolean;
 }
 
 export const configs: configsType = {
   http: httpConfig,
-  debugServer: !import.meta.env.DEV,
 };
 
 export default configs;
