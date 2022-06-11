b44119b4b7fb618a34413e90837d3f66f23d26f7
菘菘 <rrt467778@gmail.com>
2022-06-11 10:02:55 +0000

    feat: remove chinese

--- src/configs/http.ts
+++ src/configs/http.ts
@@ -1,25 +1,20 @@
 export const HttpConfig = {
-  /** request headers | 請求標頭 */
   headers: {
     'content-type': 'application/json;charset=UTF-8',
   },
-  /** relink | 是否開啟重連 */
   relink: true,
-  /** max retry | 最大重試數 */
   retry: 2,
-  /** retry delay | 重試延遲 */
   retryDelay: 5e3,
-  /** notify on success | 成功通知 */
+  /** notify on success */
   notify: false,
-  /** notify on error | 錯誤通知 */
+  /** notify on error */
   notifyError: false,
-  /** base api url | API 基本網址 */
   baseUrl: 'https://api.lipoic.org',
-  /** allow add token url | 允許加入 Token 的 URL
+  /** allow add token url
    * @type {(string|RegExp)[]}
    */
   allowTokenUrl: [/^\//],
-  /** message duration such as errors or warnings | 消息持續時間，例如錯誤或警告 (ms) */
+  /** message duration of errors or warnings */
   messageDuration: 4e3,
 };
 
--- src/http/index.ts
+++ src/http/index.ts
@@ -10,7 +10,6 @@ export interface resultErrorData {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   error?: any;
   message: string;
-  /** result config | 請求設定 */
   config: AxiosRequestConfig;
 }
 
@@ -137,17 +136,11 @@ export const uploadFile = async (
 
 declare module 'axios' {
   interface AxiosRequestConfig {
-    /** relink | 是否開啟重連 */
     relink?: boolean;
-    /** retry | 重試次數 */
     retry?: number;
-    /** retry delay | 重試延遲 */
     retryDelay?: number;
-    /** notify on success | 成功通知 */
     notify?: boolean;
-    /** notify on error | 錯誤通知 */
     notifyError?: boolean;
-    /** retry count | 重試次數 */
     __retryCount?: number;
   }
 }
