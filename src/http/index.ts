05cf1f602d64d8d50b97910235865e4d83cb92ab
猴子 <70706886+a3510377@users.noreply.github.com>
2022-06-12 15:53:37 +0800

    fix: fix src style

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 import { delay } from '@/utils';
 import HttpConfig from '@/config/http';
@@ -16,91 +16,29 @@ export interface resultErrorData {
 export type PathType = `/${string}`;
 
 export default class HttpClient {
-  private axios: AxiosInstance;
+  public http: AxiosInstance;
+  private config: Partial<HttpConfig>;
 
   constructor(config: Partial<HttpConfig> = globalConfig.http) {
-    this.axios = axios.create(config);
+    this.http = axios.create(config);
+    this.config = config;
 
-    this.axios.interceptors.request.use((_config) => {
-      _config.headers = {
-        ..._config.headers,
-        ...config.headers,
-      };
+    const { http } = this;
 
-      if (_config.retry === undefined) _config.retry = config.retry;
-      if (_config.retryDelay === undefined) _config.retryDelay = config.timeout;
+    http.interceptors.request.use(this.requestHandler.bind(this));
 
-      if (config.token) {
-        // TODO add token from stores.user.token & stores.user.type
-        _config.headers.Authorization = `Bearer ${config.token}`;
-      }
-
-      return config;
-    });
-
-    this.axios.interceptors.response.use(
-      (response) => {
-        const { config } = response;
-        // TODO add type
-        const { message, code } = response.data;
-        // TODO add notify vue component
-        if (code === 200 && config.notify) {
-          // TODO add success store message
-        } else if (config.notifyError) {
-          // TODO add error store message
-          const result: resultErrorData = {
-            data: response.data,
-            message,
-            config,
-          };
-          return Promise.reject(result);
-        }
-      },
-      async (err: { response: resultErrorData & { status: number } }) => {
-        const error = err.response;
-
-        const { config: _config, status, data } = error;
-        _config.__retryCount ||= 0;
-
-        const msg =
-          (_config.__retryCount === 0
-            ? 'error occurred:'
-            : `Reconnecting  ${_config.__retryCount} times:`) +
-          JSON.stringify({
-            baseUrl: _config.baseURL,
-            path: _config.url,
-            error: data ? data.message : error.message,
-          });
-
-        const rejectData: resultErrorData = {
-          message: msg,
-          config: _config,
-          error,
-        };
-
-        if (
-          status === 404 ||
-          !_config.relink ||
-          _config.__retryCount >= (config.retry || globalConfig.http.retry)
-        )
-          return Promise.reject(rejectData);
-
-        _config.__retryCount += 1;
-
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        return delay(config.timeout || globalConfig.http.timeout!).then(() =>
-          this.axios(_config)
-        );
-      }
+    http.interceptors.response.use(
+      this.responseHandler.bind(this),
+      this.responseErrorHandler.bind(this)
     );
   }
 
   async post(path: PathType, data?: unknown, config?: AxiosRequestConfig) {
-    return await this.axios.post(path, data, config).catch(Promise.reject);
+    return await this.http.post(path, data, config).catch(Promise.reject);
   }
 
   async get(path: PathType, params?: unknown, config?: AxiosRequestConfig) {
-    return await this.axios
+    return await this.http
       .get(path, { params, ...config })
       .catch(Promise.reject);
   }
@@ -108,12 +46,85 @@ export default class HttpClient {
   async uploadFile(path: PathType, file: File, config?: AxiosRequestConfig) {
     const param = new FormData();
     param.append('file', file, file.name);
-    return await this.axios
+    return await this.http
       .post(path, param, {
         ...config,
         headers: { 'Content-Type': 'multipart/form-data' },
       })
       .catch(Promise.reject);
+  }
+
+  protected requestHandler(_config: AxiosRequestConfig<unknown>) {
+    _config.headers = {
+      ..._config.headers,
+      ...this.config.headers,
+    };
+
+    _config.retry ??= this.config.retry;
+    _config.retryDelay ??= this.config.timeout;
+
+    if (this.config.token) {
+      // TODO add token from stores.user.token & stores.user.type
+      _config.headers.Authorization = `Bearer ${this.config.token}`;
+    }
+
+    return this.config;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  protected responseHandler(response: AxiosResponse<any, any>) {
+    const { config } = response;
+    // TODO add type
+    const { message, code } = response.data;
+    // TODO add notify vue component
+    if (code === 200 && config.notify) {
+      // TODO add success store message
+    } else if (config.notifyError) {
+      // TODO add error store message
+      const result: resultErrorData = {
+        data: response.data,
+        message,
+        config,
+      };
+      return Promise.reject(result);
+    }
+  }
+  protected async responseErrorHandler(err: {
+    response: resultErrorData & { status: number };
+  }) {
+    const error = err.response;
+
+    const { config: _config, status, data } = error;
+    _config.__retryCount ||= 0;
+
+    const msg =
+      (_config.__retryCount === 0
+        ? 'error occurred:'
+        : `Reconnecting  ${_config.__retryCount} times:`) +
+      JSON.stringify({
+        baseUrl: _config.baseURL,
+        path: _config.url,
+        error: data ? data.message : error.message,
+      });
+
+    const rejectData: resultErrorData = {
+      message: msg,
+      config: _config,
+      error,
+    };
+
+    if (
+      status === 404 ||
+      !_config.relink ||
+      _config.__retryCount >= (this.config.retry || globalConfig.http.retry)
+    )
+      return Promise.reject(rejectData);
+
+    _config.__retryCount += 1;
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return delay(this.config.timeout || globalConfig.http.timeout!).then(() =>
+      this.http(_config)
+    );
   }
 }
 
