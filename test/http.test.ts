177180baee8e13bbb22feac201c0e1ab5bdc8e0f
菘菘 <rrt467778@gmail.com>
2022-06-12 12:36:10 +0000

    test(http): more method

--- test/http.test.ts
+++ test/http.test.ts
@@ -17,26 +17,58 @@ const postResponse = {
   data: 'success created data',
 };
 
+const patchResponse = {
+  code: 200,
+  message: 'ok',
+  data: 'success updated data',
+};
+
+const deleteResponse = {
+  code: 200,
+  message: 'ok',
+  data: 'success deleted data',
+};
+
 test('get method', async () => {
   const client = new HttpClient({ baseURL });
-  const response = await client.get('/get');
+  const response = await client.get('/item');
 
   expect(response).toEqual(getResponse);
 });
 
 test('post method', async () => {
   const client = new HttpClient({ baseURL });
-  const response = await client.post('/post');
+  const response = await client.post('/item');
 
   expect(response).toEqual(postResponse);
 });
 
+test('patch method', async () => {
+  const client = new HttpClient({ baseURL });
+  const response = await client.patch('/item');
+
+  expect(response).toEqual(patchResponse);
+});
+
+test('delete method', async () => {
+  const client = new HttpClient({ baseURL });
+  const response = await client.delete('/item');
+
+  expect(response).toEqual(deleteResponse);
+});
+
 export const restHandlers = [
-  rest.get(`${baseURL}/get`, (_req, res, ctx) => {
+  rest.get(`${baseURL}/item`, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(getResponse));
   }),
-  rest.post(`${baseURL}/post`, (_req, res, ctx) => {
+  rest.post(`${baseURL}/item`, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(postResponse));
+  }),
+  rest.patch(`${baseURL}/item`, (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(patchResponse));
+  }),
+  rest.delete(`${baseURL}/item`, (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(deleteResponse));
   }),
 ];
 
