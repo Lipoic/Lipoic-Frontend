147f1e70c6682dd61cf87d471ff940acab98abd0
猴子 <70706886+a3510377@users.noreply.github.com>
2022-06-28 07:18:33 +0800

    refactor: refactor svg incon

--- src/components/SvgIcon.vue
+++ src/components/SvgIcon.vue
@@ -1,40 +1,27 @@
 <template>
-  <svg v-once aria-hidden="true" class="svg-icon-spin" :class="svgIcon">
+  <svg v-once aria-hidden="true" class="svg-icon-spin" v-bind="useAttrs()">
     <use :xlink:href="symbolId" :fill="color" />
   </svg>
 </template>
 
 <script lang="ts" setup>
-import { computed, reactive } from 'vue';
+import { computed, useAttrs } from 'vue';
 
-const props = defineProps({
-  prefix: { type: String, default: 'icon' },
-  name: { type: String, required: true },
-  color: { type: String, default: '#ccc' },
-  size: { type: String, default: 'default' },
-});
-const symbolId = computed(() => `#${props.prefix}-${props.name}`);
-const svgIcon = computed(() => ({
-  [`img-svg-size-${props.size}`]: props.size,
-}));
-const fontSize = reactive({ default: '32px', small: '20px', large: '48px' });
+// eslint-disable-next-line vue/no-setup-props-destructure
+const {
+  name,
+  prefix = 'icon',
+  color,
+} = defineProps<{
+  name: string;
+  prefix?: string;
+  color?: string;
+}>();
+
+const symbolId = computed(() => `#${prefix}-${name}`);
 </script>
 <style lang="scss" scoped>
 .svg-icon-spin {
-  width: v-bind('fontSize.default');
-  height: v-bind('fontSize.default');
-  color: v-bind(color);
   vertical-align: middle;
-  fill: v-bind(color);
-
-  &.img-svg-size-small {
-    height: v-bind('fontSize.small');
-    font-size: v-bind('fontSize.small');
-  }
-
-  &.img-svg-size-large {
-    height: v-bind('fontSize.large');
-    font-size: v-bind('fontSize.large');
-  }
 }
 </style>
