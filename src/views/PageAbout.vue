cc703eb50ce70bace2ee3a2a07c5dd89f0dd5c87
banahaker <pn0818x@outlook.com>
2022-09-09 19:57:16 +0800

    feat: about-titles

--- src/views/PageAbout.vue
+++ src/views/PageAbout.vue
@@ -1,13 +1,36 @@
-<template>
-  <MainViewVue id="about"></MainViewVue>
-</template>
-
 <script lang="ts" setup>
-import { defineAsyncComponent } from 'vue';
+import { defineAsyncComponent, ref } from 'vue';
+import BlockAboutContentsVue from '@/components/about/BlockAboutContents.vue';
 
 const MainViewVue = defineAsyncComponent(
   () => import('@/components/MainView.vue')
 );
+
+const contentList = ref({
+  who: {
+    title: 'about.whoWeAre.title',
+    content: 'about.whoWeAre.content',
+  },
+});
 </script>
 
-<style lang="scss" scoped></style>
+<template>
+  <MainViewVue id="about">
+    <div class="aboutContents">
+      <BlockAboutContentsVue
+        :title="contentList.who.title"
+        :content="contentList.who.content"
+      ></BlockAboutContentsVue>
+    </div>
+  </MainViewVue>
+</template>
+
+<style lang="scss" scoped>
+#about {
+  background-color: #fefefe;
+}
+
+.aboutContents {
+  padding: 10px 15px;
+}
+</style>
