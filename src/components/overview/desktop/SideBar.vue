4f6e30a0245ef26c1706d438779fa7551b93eb30
Kevin Zheng <4399kevin11@gmail.com>
2022-12-22 23:22:15 +0800

    feat: sidebar active animation

--- src/components/overview/desktop/SideBar.vue
+++ src/components/overview/desktop/SideBar.vue
@@ -1,13 +1,22 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+const activeIndex = ref(0);
+
+const handleChangeActive = (index: number) => {
+  activeIndex.value = index;
+};
+
 const navbarItems = [
   {
     icon: `
-      <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-        <path fill="currentColor" d="M12,10A2,2 0 0,0 10,12C10,13.11 10.9,14 12,14C13.11,14 14,13.11 14,12A2,2 0 0,0 12,10Z" />
+      <svg width="100%" height="32" viewBox="0 0 56 32" fill="none" xmlns="http://www.w3.org/2000/svg" class="block m-auto">
+        <path fill-rule="evenodd" clip-rule="evenodd"
+          d="M28 22C31.3137 22 34 19.3137 34 16C34 12.6863 31.3137 10 28 10C24.6863 10 22 12.6863 22 16C22 19.3137 24.6863 22 28 22Z"
+          fill="currentColor" />
       </svg>
     `,
     name: '總覽',
-    active: true,
   },
   {
     icon: `
@@ -17,7 +26,6 @@ const navbarItems = [
         />
     `,
     name: '我的班級',
-    active: false,
   },
   {
     icon: `
@@ -27,7 +35,6 @@ const navbarItems = [
         />
     `,
     name: '我的課堂',
-    active: false,
   },
   {
     icon: `
@@ -45,7 +52,6 @@ const navbarItems = [
       />
     `,
     name: '我的筆記',
-    active: false,
   },
   {
     icon: `
@@ -55,46 +61,87 @@ const navbarItems = [
       />
     `,
     name: '試題庫',
-    active: false,
   },
-]
+];
 </script>
 
 <template>
   <div class="sidebar w-[104px] h-full rounded-tr-[50px] flex-none">
     <div class="w-[100%] mt-[50px]">
-      <svg width="34" height="57" viewBox="0 0 34 57" fill="none" xmlns="http://www.w3.org/2000/svg"
-        class="block m-auto">
+      <svg
+        width="34"
+        height="57"
+        viewBox="0 0 34 57"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="block m-auto"
+      >
         <path
           d="M17.0094 1V1.02335C36.2564 29.4031 40.308 54.3821 17.0094 55.9958V56C17.0074 55.9995 17.0055 55.9989 17.0035 55.9983V55.9996L17 55.9985L16.9965 55.9996V55.9983C16.9945 55.9988 16.9926 55.9994 16.9906 56V55.9958C-6.30797 54.3821 -2.25643 29.4031 16.9906 1.02335V1C16.9927 1.00332 16.9945 1.00644 16.9965 1.00968V1.0021C16.9975 1.00407 16.9988 1.00604 17 1.00799C17.0012 1.00604 17.0024 1.00407 17.0035 1.0021V1.00968C17.0055 1.00644 17.0074 1.00332 17.0094 1ZM17.0424 10.3408C17.0424 10.3408 17.0233 10.3754 17.0094 10.4005V10.4258C17.0094 10.4258 17.0058 10.4195 17.0035 10.4154V10.4234C17.0035 10.4234 17.0013 10.4195 17 10.4171C16.9987 10.4195 16.9965 10.4234 16.9965 10.4234V10.4154C16.9942 10.4194 16.9906 10.4258 16.9906 10.4258V10.4005C16.9767 10.3754 16.9576 10.3408 16.9576 10.3408C16.9576 10.3408 14.2123 14.9912 11.4949 20.6659C12.9124 21.9584 15.1806 23.4545 17 23.4525C18.8194 23.4545 21.0876 21.9584 22.5051 20.6659C19.7877 14.9912 17.0424 10.3408 17.0424 10.3408ZM23.7874 24.6523C23.7874 24.6523 18.9864 28.0722 17.0023 28.0722L17 28.0719L16.9977 28.0722C15.0136 28.0722 10.2126 24.6523 10.2126 24.6523C10.2126 24.6523 6.74142 31.6015 6.28493 35.5007C5.36371 43.3694 10.1204 50.3514 16.9965 50.3574H17L17.0035 50.3572C23.8796 50.3511 28.6363 43.3694 27.7151 35.5007C27.2586 31.6015 23.7874 24.6523 23.7874 24.6523Z"
-          fill="url(#paint0_linear_485_1488)" stroke="#4D4D4D" stroke-width="0.5" stroke-linejoin="round" />
+          fill="url(#paint0_linear_485_1488)"
+          stroke="#4D4D4D"
+          stroke-width="0.5"
+          stroke-linejoin="round"
+        />
         <defs>
-          <linearGradient id="paint0_linear_485_1488" x1="0.704457" y1="0.700756" x2="12.742" y2="60.4408"
-            gradientUnits="userSpaceOnUse">
+          <linearGradient
+            id="paint0_linear_485_1488"
+            x1="0.704457"
+            y1="0.700756"
+            x2="12.742"
+            y2="60.4408"
+            gradientUnits="userSpaceOnUse"
+          >
             <stop stop-color="#5EFFF2" />
             <stop offset="1" stop-color="#B54CFF" />
           </linearGradient>
         </defs>
       </svg>
-      <svg width="59" height="5" viewBox="0 0 59 5" fill="none" xmlns="http://www.w3.org/2000/svg"
-        class="block m-auto mt-[50px]">
+      <svg
+        width="59"
+        height="5"
+        viewBox="0 0 59 5"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="block m-auto mt-[50px]"
+      >
         <rect width="59" height="5" rx="2.5" fill="#7B89B8" />
       </svg>
     </div>
 
-    <div class="mt-5" v-for="(item, index) in navbarItems" :key="index">
-      <a class="hover:cursor-pointer text-[#3F4759] dark:text-white">
-        <!-- <div class="" v-html="item.icon"></div> -->
-        <svg
-          width="64"
-          height="32"
-          viewBox="0 0 64 32"
-          fill="inherit"
-          xmlns="http://www.w3.org/2000/svg"
-          class="block m-auto mt-[40px]"
-          v-html="item.icon"
-        ></svg>
-        <p class="text-center text-sm mt-[10px]">{{ item.name }}</p>
+    <div
+      :class="['mt-[45px]', 'sidebar-item', activeIndex === index && 'active']"
+      v-for="(item, index) in navbarItems"
+      :key="index"
+    >
+      <a
+        class="hover:cursor-pointer text-[#3F4759] dark:text-white"
+        @click="handleChangeActive(index)"
+      >
+        <div class="icon-layout">
+          <div
+            :class="[
+              'overlay',
+              activeIndex === index && 'active',
+            ]"
+          ></div>
+          <svg
+            width="64"
+            height="32"
+            viewBox="0 0 64 32"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            :class="[
+              'm-auto',
+              'icon',
+              activeIndex === index && 'active',
+            ]"
+            v-html="item.icon"
+          ></svg>
+        </div>
+        <p class="text-center text-sm mt-[10px]">
+          {{ item.name }}
+        </p>
       </a>
     </div>
   </div>
@@ -107,27 +154,100 @@ const navbarItems = [
   box-sizing: border-box;
   position: absolute;
 
-  background: linear-gradient(0deg, rgba(0, 90, 193, 0.08), rgba(0, 90, 193, 0.08)), #FEFBFF;
-  border-top: 1px solid #F1F3F9;
-  border-right: 1px solid #F1F3F9;
+  background: linear-gradient(
+      0deg,
+      rgba(0, 90, 193, 0.08),
+      rgba(0, 90, 193, 0.08)
+    ),
+    #fefbff;
+  border-top: 1px solid #f1f3f9;
+  border-right: 1px solid #f1f3f9;
   border-radius: 0px 50px 0px 0px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
   transition: 300ms ease background;
+
+  .dark & {
+    box-sizing: border-box;
+    position: absolute;
+
+    border-top: 1px solid #353556;
+    border-right: 1px solid #353556;
+    border-radius: 0px 50px 0px 0px;
+
+    background: linear-gradient(
+        0deg,
+        rgba(173, 198, 255, 0.08),
+        rgba(173, 198, 255, 0.08)
+      ),
+      #1b1b1f;
+  }
 }
 
-.dark .sidebar {
-  box-sizing: border-box;
-  position: absolute;
+// sidebar style and animation
+.sidebar-item {
+  transition: ease 300ms all;
+  /* background-color: rgba(0, 0, 0, 0.2); */
 
-  border-top: 1px solid #353556;
-  border-right: 1px solid #353556;
-  border-radius: 0px 50px 0px 0px;
+  &:hover {
+    .icon-layout {
+      background-color: rgba(81, 92, 152, 0.1);
+    }
+  }
 
-  background: linear-gradient(
-      0deg,
-      rgba(173, 198, 255, 0.08),
-      rgba(173, 198, 255, 0.08)),
-    #1b1b1f;
+  &:active {
+    font-weight: bold;
+
+    .icon-layout {
+      background-color: rgba(81, 92, 152, 0.3);
+    }
+  }
+
+  &.active {
+    font-weight: bold;
+
+    &:active {
+      font-weight: normal;
+    }
+  }
+
+  .icon-layout {
+    position: relative;
+    margin-left: 1.2rem;
+    margin-right: 1.2rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 99rem;
+    transition: ease 300ms all;
+
+    .overlay {
+      position: absolute;
+      margin: 0 auto;
+      width: 2rem;
+      height: 2rem;
+      border-radius: 99rem;
+      /* background-color: #515c98; */
+      transition: ease 200ms all;
+
+      &.active {
+        width: 100%;
+        background-color: #515c98;
+      }
+    }
+
+    .icon {
+      position: relative;
+      height: 2rem;
+      border-radius: 99rem;
+      transition: ease 200ms all;
+
+      &.active {
+        color: #dbe2f9;
+      }
+    }
+  }
 }
 </style>
