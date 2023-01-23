1d7655c99064c9d480f97b00a5d0bcdf336cf4b7
HACO <51485352+MRHACO@users.noreply.github.com>
2023-01-23 22:46:02 +0800

    Update src/components/Desktop/Overview/AppBar.vue
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/components/Desktop/Overview/AppBar.vue
+++ src/components/Desktop/Overview/AppBar.vue
@@ -28,7 +28,7 @@ const settingsStore = useSettingsStore();
         />
       </div>
     </form>
-    <div class="flex-1 text-right mt-7">
+    <div class="mt-7 flex-1 text-right">
       <button class="mx-[48px]" @click="settingsStore.toggleTheme">
         <!-- auto icon -->
         <svg
