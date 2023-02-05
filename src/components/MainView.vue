d6d92ed61e5c6d96b66e329032152c3e268bc822
SiongSng <rrt467778@gmail.com>
2023-02-05 22:37:38 +0800

    hotfix: display bug

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,7 +1,15 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="bg-[#FCFBFF] dark:bg-[#1B1B1F]">
+  <div class="main bg-[#FCFBFF] dark:bg-[#1B1B1F]">
     <slot />
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import '@/scss/global.scss';
+.main {
+  min-height: 100%;
+  display: flex;
+}
+</style>
