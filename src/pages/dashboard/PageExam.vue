0642f3a8a34b33a78d6443c59af7fb54c1fe1dcb
菘菘 <rrt467778@gmail.com>
2023-06-12 21:36:00 +0800

    fix: adjust content block position and size
    Co-Authored-By: LAZPbanahaker <pn0818x@outlook.com>

--- src/pages/dashboard/PageExam.vue
+++ src/pages/dashboard/PageExam.vue
@@ -1,7 +1,11 @@
 <script lang="ts" setup></script>
 
 <template>
-  <p>此功能正在製作中</p>
+  <div
+    class="absolute right-0 top-[77px] h-[calc(100svh-77px)] w-[calc(100svw-104px)]"
+  >
+    <p>此功能正在製作中</p>
+  </div>
 </template>
 
 <style lang="scss" scoped>
